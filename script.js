9050e70ae7adedfbac3d2cf99a926b6ba2e6aef8
Jerus Zion Sanqui <87622389+Jezii12@users.noreply.github.com>
2025-02-13 17:30:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,181 @@
+$(document).ready(function () {
+    $('.container').mouseenter(function () {
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow');
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow');
+    });
+});
+
+function openModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+function openModal2() {
+    var modal = document.getElementById("myModal2");
+    modal.style.display = "block";
+}
+
+function closeModal2() {
+    var modal = document.getElementById("myModal2");
+    modal.style.display = "none";
+}
+
+function openModal3() {
+    var modal = document.getElementById("myModal3");
+    modal.style.display = "block";
+}
+
+function closeModal3() {
+    var modal = document.getElementById("myModal3");
+    modal.style.display = "none";
+}
+
+function openModal4() {
+    var modal = document.getElementById("myModal4");
+    modal.style.display = "block";
+}
+
+function closeModal4() {
+    var modal = document.getElementById("myModal4");
+    modal.style.display = "none";
+}
+
+function openModal5() {
+    var modal = document.getElementById("myModal5");
+    modal.style.display = "block";
+}
+
+function closeModal5() {
+    var modal = document.getElementById("myModal5");
+    modal.style.display = "none";
+}
+
+function openModal6() {
+    var modal = document.getElementById("myModal6");
+    modal.style.display = "block";
+}
+
+function closeModal6() {
+    var modal = document.getElementById("myModal6");
+    modal.style.display = "none";
+}
+
+function openModal7() {
+    var modal = document.getElementById("myModal7");
+    modal.style.display = "block";
+}
+
+function closeModal7() {
+    var modal = document.getElementById("myModal7");
+    modal.style.display = "none";
+}
+
+function openModal8() {
+    var modal = document.getElementById("myModal8");
+    modal.style.display = "block";
+}
+
+function closeModal8() {
+    var modal = document.getElementById("myModal8");
+    modal.style.display = "none";
+}
+
+function openModal9() {
+    var modal = document.getElementById("myModal9");
+    modal.style.display = "block";
+}
+
+function closeModal9() {
+    var modal = document.getElementById("myModal9");
+    modal.style.display = "none";
+}
+
+function openModal10() {
+    var modal = document.getElementById("myModal10");
+    modal.style.display = "block";
+}
+
+function closeModal10() {
+    var modal = document.getElementById("myModal10");
+    modal.style.display = "none";
+}
+
+function openModal11() {
+    var modal = document.getElementById("myModal11");
+    modal.style.display = "block";
+}
+
+function closeModal11() {
+    var modal = document.getElementById("myModal11");
+    modal.style.display = "none";
+}
+
+function openModal12() {
+    var modal = document.getElementById("myModal12");
+    modal.style.display = "block";
+}
+
+function closeModal12() {
+    var modal = document.getElementById("myModal12");
+    modal.style.display = "none";
+}
+
+function openModal13() {
+    var modal = document.getElementById("myModal13");
+    modal.style.display = "block";
+}
+
+function closeModal13() {
+    var modal = document.getElementById("myModal13");
+    modal.style.display = "none";
+}
+
+function openModal14() {
+    var modal = document.getElementById("myModal14");
+    modal.style.display = "block";
+}
+
+function closeModal14() {
+    var modal = document.getElementById("myModal14");
+    modal.style.display = "none";
+}
+
+function openModal15() {
+    var modal = document.getElementById("myModal15");
+    modal.style.display = "block";
+}
+
+function closeModal15() {
+    var modal = document.getElementById("myModal15");
+    modal.style.display = "none";
+}
+
+function openModal16() {
+    var modal = document.getElementById("myModal16");
+    modal.style.display = "block";
+}
+
+function closeModal16() {
+    var modal = document.getElementById("myModal16");
+    modal.style.display = "none";
+}
+
+function openModal17() {
+    var modal = document.getElementById("myModal17");
+    modal.style.display = "block";
+}
+
+function closeModal17() {
+    var modal = document.getElementById("myModal17");
+    modal.style.display = "none";
+}
